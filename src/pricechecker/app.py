b590669692cc54700047fb8aa8466dab9122a5c3
SlimedLeviathan <cantonsteven6@gmail.com>
2023-03-17 13:19:24 -0400

    walmart

--- src/pricechecker/app.py
+++ src/pricechecker/app.py
@@ -8,7 +8,6 @@ from .serpApiTest import search
 from toga.constants import RED, GREEN, YELLOW
 import webbrowser
 
-
 class PriceChecker(toga.App):
 
     def startup(self):
@@ -21,15 +20,25 @@ class PriceChecker(toga.App):
         """
         main_box = toga.Box(style = Pack(direction = COLUMN))
 
-        search_box = toga.Box(style = Pack(direction = ROW, padding = 5))
+        inputPadding = 2
 
-        input_label = toga.Label("Search on Ebay: ", style = Pack(padding = (0,5)))
+        input_box = toga.Box(style = Pack(direction = COLUMN))
+
+        search_box = toga.Box(style = Pack(direction = ROW, padding = inputPadding))
+
+        input_label = toga.Label("Search on", style = Pack(padding = (0,5)))
+        self.APIDrop = toga.Selection(items = ['Ebay', 'Google Shopping', 'Google Products', 'Walmart', 'Home Depot'], style = Pack(width = 120))
+        input_label2 = toga.Label(": ", style = Pack(padding = (0,5)))
         self.search_input = toga.TextInput(style = Pack(flex = 1))
 
         search_box.add(input_label)
+        search_box.add(self.APIDrop)
+        search_box.add(input_label2)
         search_box.add(self.search_input)
+
+        input_box.add(search_box)
         
-        price_box = toga.Box(style = Pack(direction = ROW, padding = 5))
+        price_box = toga.Box(style = Pack(direction = ROW, padding = inputPadding))
 
         price_label = toga.Label("What are you looking to pay for it? : ", style = Pack(padding = (0,5)))
         self.price_input = toga.TextInput(style = Pack(flex = 1))
@@ -37,12 +46,24 @@ class PriceChecker(toga.App):
         price_box.add(price_label)
         price_box.add(self.price_input)
 
-        button_box = toga.Box(style = Pack(direction = ROW, padding = 5))
+        input_box.add(price_box)
+
+        leeway_box = toga.Box(style = Pack(direction = ROW, padding = inputPadding))
+
+        leeway_label = toga.Label("What amount of leeway would you like? : ", style = Pack(padding = (0,5)))
+        self.leeway_input = toga.TextInput(style = Pack(flex = 1))
+
+        leeway_box.add(leeway_label)
+        leeway_box.add(self.leeway_input)
+
+        input_box.add(leeway_box)
+
+        button_box = toga.Box(style = Pack(direction = ROW, padding = 2))
 
         self.resultButtonList = []
 
-        self.search_button = toga.Button('Search on Ebay', on_press = self.search, style = Pack(padding = 5))
-        self.search_under_button = toga.Button('Search Below Price Input', on_press = self.search_under, style = Pack(padding = 5))
+        self.search_button = toga.Button('Search', on_press = self.search, style = Pack(padding = 2))
+        self.search_under_button = toga.Button('Search Below Price Input', on_press = self.search_under, style = Pack(padding = 2))
 
         button_box.add(self.search_button)
         button_box.add(self.search_under_button)
@@ -50,10 +71,9 @@ class PriceChecker(toga.App):
         self.resultBox = toga.Box(style = Pack(direction = COLUMN))
         self.resultScroll = toga.ScrollContainer(style = Pack(direction = COLUMN), horizontal = False, content = self.resultBox)
 
-        main_box.add(search_box)
-        main_box.add(price_box)
+        main_box.add(input_box)
         main_box.add(button_box)
-        main_box.add(self.resultBox)
+        main_box.add(self.resultScroll)
 
         self.main_window = toga.MainWindow(title=self.formal_name)
         self.main_window.content = main_box
@@ -64,17 +84,16 @@ class PriceChecker(toga.App):
         for result in self.resultButtonList:
             self.resultBox.remove(result)
         
-        self.resultsList = search(self.search_input.value)
+        self.resultsList = search(self.APIDrop.value, self.search_input.value)
 
         self.get_results()
 
     def search_under(self, *args):
 
-        for _ in range(len(self.resultButtonList)):
-            self.resultBox.remove(self.resultButtonList[-1])
-            self.resultButtonList.pop()
+        for result in self.resultButtonList:
+            self.resultBox.remove(result)
 
-        self.resultsList = search(self.search_input.value, float(self.price_input.value))
+        self.resultsList = search(self.APIDrop.value, self.search_input.value, float(self.price_input.value))
 
         self.get_results()
 
@@ -82,11 +101,19 @@ class PriceChecker(toga.App):
 
         leeway = 50
 
+        if self.leeway_input.value != '':
+            leeway = int(self.leeway_input.value)
+
         self.resultButtonList = []
 
         if self.price_input.value != "":
             for result in self.resultsList:
-                if int(self.price_input.value) - leeway < result['price']:
+
+                # If the price is not specified, keep the color white
+                if type(result['price']) == str:
+                    self.resultButtonList.append(toga.Button(result['message'], on_press = self.link, style = Pack()))
+
+                elif int(self.price_input.value) - leeway < result['price']:
                     self.resultButtonList.append(toga.Button(result['message'], on_press = self.link, style = Pack(background_color = RED)))
 
                 elif int(self.price_input.value) + leeway > result['price']:
--- src/pricechecker/serpApiTest.py
+++ src/pricechecker/serpApiTest.py
@@ -1,6 +1,10 @@
 from serpapi import GoogleSearch
 
-def search(item, price = None):
+def search(API, item, price = None):
+
+    return searchDict[API](item, price)
+
+def ebaySearch(item, price = None):
     params = {
     "engine": "ebay",
     "_nkw" : f"{item}",
@@ -17,40 +21,143 @@ def search(item, price = None):
 
     for result in results['organic_results']:
 
-        if price == None:
-            makeMessage(result, resultsList)
+        itemPrice = ebayPrices(result)
 
-        else:
-            try:
-                if result["price"]["from"]["extracted"] < price:
-                    makeMessage(result, resultsList)
+        if price == None or itemPrice == 'Price Not Found':
+            makeMessage(result['title'], itemPrice, result['link'], resultsList)
 
-            except:
-                try:
-                    if result["price"]["extracted"] < price:
-                        makeMessage(result, resultsList)
-
-                except:
-                    makeMessage(result, resultsList)
+        elif itemPrice < price:
+            makeMessage(result['title'], itemPrice, result['link'], resultsList)
 
     return resultsList
 
-def makeMessage(result, resultsList):
+def ebayPrices(result):
+
+    try:
+        try:
+            return f'${result["price"]["from"]["extracted"]} to ${result["price"]["to"]["extracted"]}'
+
+        except:
+            return f'${result["price"]["extracted"]}'
+
+    except:
+        return "Price Not Found"
+
+def HDSearch(item, price = None):
+    params = {
+    "engine": "ebay",
+    "_nkw" : f"{item}",
+    "ebay_domain": "ebay.com",
+    "api_key": "7882222ae7ae04d88939b426f6170b90a4c6f8fde079a51b27b715f84aef8800"
+    }
+
+    search = GoogleSearch(params)
+    results = search.get_dict()
+
+
+    resultsList = []
+    # message += results['search_metadata']['ebay_url']
+
+    for result in results['organic_results']:
+
+        itemPrice = ebayPrices(result)
+
+        if price == None or itemPrice == 'Price Not Found':
+            makeMessage(result['title'], itemPrice, result['link'], resultsList)
+
+        elif itemPrice < price:
+            makeMessage(result['title'], itemPrice, result['link'], resultsList)
+
+    return resultsList
+
+def GSSearch(item, price = None):
+    params = {
+    "engine": "ebay",
+    "_nkw" : f"{item}",
+    "ebay_domain": "ebay.com",
+    "api_key": "7882222ae7ae04d88939b426f6170b90a4c6f8fde079a51b27b715f84aef8800"
+    }
+
+    search = GoogleSearch(params)
+    results = search.get_dict()
+
+
+    resultsList = []
+    # message += results['search_metadata']['ebay_url']
+
+    for result in results['organic_results']:
+
+        itemPrice = ebayPrices(result)
+
+        if price == None or itemPrice == 'Price Not Found':
+            makeMessage(result['title'], itemPrice, result['link'], resultsList)
+
+        elif itemPrice < price:
+            makeMessage(result['title'], itemPrice, result['link'], resultsList)
+
+    return resultsList
+
+def GPSearch(item, price = None):
+    params = {
+    "engine": "ebay",
+    "_nkw" : f"{item}",
+    "ebay_domain": "ebay.com",
+    "api_key": "7882222ae7ae04d88939b426f6170b90a4c6f8fde079a51b27b715f84aef8800"
+    }
+
+    search = GoogleSearch(params)
+    results = search.get_dict()
+
+
+    resultsList = []
+    # message += results['search_metadata']['ebay_url']
+
+    for result in results['organic_results']:
+
+        itemPrice = ebayPrices(result)
+
+        if price == None or itemPrice == 'Price Not Found':
+            makeMessage(result['title'], itemPrice, result['link'], resultsList)
+
+        elif itemPrice < price:
+            makeMessage(result['title'], itemPrice, result['link'], resultsList)
+
+    return resultsList
+
+def walmartSearch(item, price = None):
+    params = {
+    "engine": "walmart",
+    "query" : f"{item}",
+    "api_key": "7882222ae7ae04d88939b426f6170b90a4c6f8fde079a51b27b715f84aef8800"
+    }
+
+    search = GoogleSearch(params)
+    results = search.get_dict()
+
+
+    resultsList = []
+    # message += results['search_metadata']['ebay_url']
+
+    for result in results['organic_results']:
+
+        itemPrice = result['primary_offer']['offer_price']
+
+        if price == None or itemPrice == 'Price Not Found':
+            makeMessage(result['title'], itemPrice, result['product_page_url'], resultsList)
+
+        elif itemPrice < price:
+            makeMessage(result['title'], itemPrice, result['product_page_url'], resultsList)
+
+    return resultsList
+
+def makeMessage(title, price, link, resultsList):
     
     message = ''
 
-    try:
-        message += f'\n{result["title"]} '
-        # message += result['condition']
+    message += title
+        
+    message += f' {price}'
 
-        try:
-            message += f'${result["price"]["from"]["extracted"]} to ${result["price"]["to"]["extracted"]}'
-            resultsList.append({'message' : message, 'link' : result['link'], 'price' : result['price']['to']['extracted']})
+    resultsList.append({'message' : message, 'link' : link, 'price' : price})
 
-        except:
-            message += f'${result["price"]["extracted"]}'
-            resultsList.append({'message' : message, 'link' : result['link'], 'price' : result['price']['extracted']})
-
-    except:
-        message += "Price Not Found"
-        resultsList.append({'message' : message, 'link' : result['link'], 'price' : 'Price Not Found'})
+searchDict = {'Ebay' : ebaySearch, 'Google Shopping' : GSSearch, 'Google Products' : GPSearch, 'Walmart' : walmartSearch, 'Home Depot' : HDSearch}
